9248e88b8231407432a481caac6ed943c2ffc237
DESKTOP-P4LPODA\l1j1g <l1j1g5@naver.com>
2022-02-14 00:38:54 +0900

    quiz01 커밋

--- src/main/java/com/test/lesson04/Lesson04Quiz01.java
+++ src/main/java/com/test/lesson04/Lesson04Quiz01.java
@@ -2,6 +2,8 @@ package com.test.lesson04;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,22 +13,36 @@ import javax.servlet.http.HttpServletResponse;
 import com.test.common.MysqlService;
 
 @WebServlet("/lesson04/quiz01")
-public class Lesson04Quiz01 extends HttpServlet{
-	
+public class Lesson04Quiz01 extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setCharacterEncoding("utf-8");
 		response.setContentType("text/plain");
-		PrintWriter out = response.getWriter();
 		
-		MysqlService mysql = MysqlService.getInstance();
-		mysql.connection();
-		
-		String selectQuery = ""
-				insert into `real_estate` (`sellerId`, `address`, `area`, `type`, `price`, `rentPrice`)
-				values (3, '혜라펠리스 101동 5305호', 350, '매매', 1500000, null);
-		
-		select *  from `real_estate`
-		order by `id` desc
-		limit 10;
-		
+		MysqlService mysqlService = MysqlService.getInstance(); 
+		mysqlService.connection(); // DB 연결
+
+		// -- 쿼리 수행
+		try {
+			String insertQuery = "insert into `real_estate` (`realtorId`, `address`, `area`, `type`, `price`, `rentPrice`) "
+					+ "values (3, '헤라펠리스 101동 5305호', 350, '매매', 1500000, NULL)";
+			mysqlService.update(insertQuery);
+			
+			// 출력
+			PrintWriter out = response.getWriter();
+			
+			String selectQuery = "select `address`, `area`, `type` from `real_estate` order by `id` DESC limit 10";
+			ResultSet result = mysqlService.select(selectQuery);
+			
+			while (result.next()) {
+				String address = result.getString("address");
+				Integer area = result.getInt("area");
+				String type = result.getString("type");
+				out.println("매물 주소: " + address + ", 면적: " + area + ", 타입: " + type);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		mysqlService.disconnect(); // DB 해제
 	}
 }
